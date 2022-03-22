de39f8abebd5726784828d240534ecf0ee114a0d
Derek <94083227+DerekPistorius@users.noreply.github.com>
2022-03-21 18:12:27 -0600

    Add files via upload

---
+++ WizardingWorld.playground/Contents.swift
@@ -0,0 +1,32 @@
+let firstName = "Draco"
+let lastName = "Malfoy"
+let birthYear = "6/5/1980"
+let nationality = "British"
+let bloodStatus = "Pure-blood"
+let gender = "Male"
+var height = "5 feet 9 inches"
+var weight = "138 lbs"
+var appearedInHarryPotterMovies = true
+var appeardInFantasticBeasts = false
+var wasDumbledoresArmy = false
+var wasDeathEater = true
+var howCrucial = "Very crucial"
+var actorRating = 10
+var reasonForFavorite = "I am not a huge Hary Potter fan, however Draco was a great bad guy!"
+
+print(firstName + " " + lastName)
+print(birthYear)
+print(nationality)
+print(bloodStatus)
+print(gender)
+print(height)
+print(weight)
+print("appeared in Harry Potter Movies?", appearedInHarryPotterMovies)
+print("appeared in Fantastic Beasts?", appeardInFantasticBeasts)
+print("was in Dumbledores Army?", wasDumbledoresArmy)
+print("was a Death Eater?", wasDeathEater)
+print("how crucial of a role did Draco Malfoy have?", howCrucial)
+print("Out of 10, how good was the actor that played Draco Malfoy?", actorRating)
+print(reasonForFavorite)
+
+
